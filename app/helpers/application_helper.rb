bd557abcf0fb22374dc3202d98d985ce6f9036e1
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-08-24 01:33:30 +0900

    Fix: QRコードをPNG化&リサイズできるよう修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
   require "rqrcode"
   require "rqrcode_png"
+  require "chunky_png"
 
   # QRコードを生成するメソッド
-  def qrcode(url, size)
+  def qrcode_tag(url, options = {})
     if Rails.env.production?
       # 本番環境のみの処理
       qrcode = RQRCode::QRCode.new("https://qr-treasure-hunt.herokuapp.com#{url}")
@@ -11,14 +12,27 @@ module ApplicationHelper
       # 開発環境のみの処理
       qrcode = RQRCode::QRCode.new(url)
     end
-    svg = qrcode.as_svg(
-      color: "000",
-      shape_rendering: "crispEdges",
-      module_size: size,
-      standalone: true,
-      use_path: true
-    ).html_safe
+    return ChunkyPNG::Image.from_datastream(qrcode.as_png.resize(150,150).to_datastream).to_data_url
   end
+
+  # def qrcode(url, size)
+  #   if Rails.env.production?
+  #     # 本番環境のみの処理
+  #     qrcode = RQRCode::QRCode.new("https://qr-treasure-hunt.herokuapp.com#{url}")
+  #   elsif Rails.env.development?
+  #     # 開発環境のみの処理
+  #     qrcode = RQRCode::QRCode.new(url)
+  #   end
+  #   svg = qrcode.as_svg(
+  #     color: "000",
+  #     shape_rendering: "crispEdges",
+  #     # module_size: size,
+  #     width: "#{size}px",
+  #     height: "#{size}px",
+  #     standalone: true,
+  #     use_path: true
+  #   ).html_safe
+  # end
 
   # bootstrapのアイコンを生成するメソッド
   def icon(icon_name)
