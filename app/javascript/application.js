387c9baf3340b402cbaf614bf54922d5e763f257
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-08-26 21:23:18 +0900

    Fix: 微修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,7 +4,7 @@ import "./controllers"
 import * as bootstrap from "bootstrap"
 
 // ES6 import
-import jsQR from "jsqr";
+// import jsQR from "jsqr";
 
 // CommonJS require
 const jsQR = require("jsqr");
