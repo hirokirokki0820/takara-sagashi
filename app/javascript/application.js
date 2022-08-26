86f5c7d34f6afb94258fcd07e6661e40abe779c7
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-08-26 21:35:31 +0900

    Fix: 微修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,9 +2,3 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-
-// ES6 import
-// import jsQR from "jsqr";
-
-// CommonJS require
-const jsQR = require("jsqr");
